f88ea4f6343481ee1a814db10a2b2570acd7e3e9
Clos76 <robertoccg@proton.me>
2025-07-20 23:35:56 -0700

    show only coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
-import RecommendedSection from "@/components/layout/RecommendedSection";
-import Hero from "../components/layout/Hero";
-import InfoCards from "@/components/layout/InfoCards";
-import Neighborhoods from "@/components/layout/Neighborhoods";
-import ContactForm from "@/components/layout/ContactForm";
+// import RecommendedSection from "@/components/layout/RecommendedSection";
+// import Hero from "../components/layout/Hero";
+// import InfoCards from "@/components/layout/InfoCards";
+// import Neighborhoods from "@/components/layout/Neighborhoods";
+// import ContactForm from "@/components/layout/ContactForm";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      {/* Header should be outside of grid so it's full width */}
-      <Hero/>
-
-      <RecommendedSection/>
-
-      <InfoCards/>
-
-      <Neighborhoods/>
-
-
-      <ContactForm/>
-
-      {/* Hero should be full width too */}
-      
-
-      {/* Main Content */}
-      <main className="max-w-screen-xl mx-auto px-4 sm:px-8 py-12">
-        <h1 className="text-3xl font-semibold mt-9">Coming Soon</h1>
-        {/* More content here */}
+     <>
+      <Head>
+        <title>Rosarito Real Estate - Coming Soon</title>
+        <meta name="description" content="Our Rosarito Real Estate site is launching soon. Stay tuned!" />
+      </Head>
+      <main className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+        <h1 className="text-4xl font-semibold">Coming Soon</h1>
       </main>
-    </div>
+    </>
   );
 }
