d81fb0ad0eb30b3472f8e5f6f3eeac496b94e48a
Clos76 <robertoccg@proton.me>
2025-08-21 14:06:54 -0700

    added headers for security

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,40 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  // Add more Next.js options here if needed
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // applies to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data: https://rosaritoresorts.com;"
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          },
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
