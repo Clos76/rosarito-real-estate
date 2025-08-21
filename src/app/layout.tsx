22df71566e8fb7b85bae762e38a43c5da9dde905
Clos76 <robertoccg@proton.me>
2025-08-21 12:28:54 -0700

    Added vercel analytics, changed email domain to point to vecel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
+import { Analytics } from "@vercel/analytics/next";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
+        {/* Vercel Analytics site-wide */}
+        <Analytics />
       </body>
     </html>
   );
